24bbef274aa2f36c36ed9040ef411f43a8abf64f
David Banham <david@banham.id.au>
2017-11-01 10:43:01 +1100

    Throw error objects rather than strings

--- test.js
+++ test.js
@@ -2,64 +2,64 @@ require('./index.js');
 const express = require('express');
 const supertest = require('supertest');
 
-describe('express-async-errors', function(){
-  it('propagates routes errors to error handler', function(){
-    const app = express();       
-
-    app.get('/test', async function(){
-      throw 'error'
-    });
-
-    app.use(function(err, req, res, next){
-      res.status(495);
-      res.end();
-    });
-
-    return supertest(app)
-      .get('/test')
-      .expect(495)
-  });
-
-  it('propagates regular middleware errors too', function(){
+describe('express-async-errors', () => {
+  it('propagates routes errors to error handler', () => {
     const app = express();
 
-    app.use(async function(){
-      throw 'error';
+    app.get('/test', async () => {
+      throw new Error('error');
     });
 
-    app.get('/test', async function(){
-      throw 'error'
-    });
-
-
-    app.use(function(err, req, res, next){
+    app.use((err, req, res, next) => {
       res.status(495);
       res.end();
     });
 
     return supertest(app)
       .get('/test')
-      .expect(495)
+      .expect(495);
   });
 
-  it('and propagates error middleware errors too', function(){
+  it('propagates regular middleware errors too', () => {
     const app = express();
 
-    app.get('/test', async function(){
-      throw 'error'
+    app.use(async () => {
+      throw new Error('error');
     });
 
-    app.use(async function(err, req, res, next){
-      throw 'error';
+    app.get('/test', async () => {
+      throw new Error('error');
     });
 
-    app.use(function(err, req, res, next){
+
+    app.use((err, req, res, next) => {
       res.status(495);
       res.end();
     });
 
     return supertest(app)
       .get('/test')
-      .expect(495)
-  })
+      .expect(495);
+  });
+
+  it('and propagates error middleware errors too', () => {
+    const app = express();
+
+    app.get('/test', async () => {
+      throw new Error('error');
+    });
+
+    app.use(async (err, req, res, next) => {
+      throw new Error('error');
+    });
+
+    app.use((err, req, res, next) => {
+      res.status(495);
+      res.end();
+    });
+
+    return supertest(app)
+      .get('/test')
+      .expect(495);
+  });
 });
